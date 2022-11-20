308d4e12b0805eb3ff2cc47dcf50362b723d146d
Jake Bills <jakebills1@gmail.com>
2022-11-20 14:16:17 -0700

    adds basic interactive interface

--- main.rb
+++ main.rb
@@ -1,1 +1,34 @@
-puts "hello world"
+require_relative './babysitter'
+
+babysitter = Babysitter.new
+puts 'Welcome to your night of babysitting. Ready to clock in?'
+while !@clock_in_time_entered
+  begin
+    puts "Enter hour started:"
+    hour = gets.chomp
+    puts "AM or PM?"
+    ampm = gets.chomp
+    babysitter.clock_in("#{hour} #{ampm}")
+    @clock_in_time_entered = true
+  rescue ArgumentError => e
+    puts e.message
+    puts "Try Again"
+  end
+end
+while !@clock_out_time_entered
+  begin
+    puts "Enter hour ended:"
+    hour = gets.chomp
+    puts "AM or PM?"
+    ampm = gets.chomp
+    babysitter.clock_out("#{hour} #{ampm}")
+    @clock_out_time_entered = true
+  rescue ArgumentError => e
+    puts e.message
+    puts "Try Again"
+  end
+end
+
+puts "You have earned #{babysitter.calculate_nightly_pay}"
+
+
